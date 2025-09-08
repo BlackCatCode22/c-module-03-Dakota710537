d90d3e252b4cf6c6ed55bb83d42e9163b543e54a
Dakota710537 <dakotafernandez1025@gmail.com>
2025-09-08 12:18:47 -0700

    Add files via upload

---
+++ 2DArray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+
+    int numberGrid[3][2] = {
+        {1,2},
+        {3,4},
+        {5,6},
+    };
+    cout <<numberGrid[0][1]<<endl;
+
+    return 0;
+}
